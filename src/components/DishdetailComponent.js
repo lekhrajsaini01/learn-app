15e99d92f4612bd758b1e6a1cc51489060df9b84
lekhrajsaini01 <lekhrajsaini7000@gmail.com>
2021-05-10 02:09:52 +0530

    redux-thunk, fetch, json server, animations

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -18,6 +18,7 @@ import{ Card,
 import {Link} from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -39,7 +40,7 @@ class CommentForm extends Component{
     }
     handleSubmit(values){
         this.toggleModal();
-        this.props.addComment(this.props.dishId,values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId,values.rating, values.author, values.comment);
         console.log("current State is:"+JSON.stringify(values));
     }
 
@@ -116,7 +117,7 @@ class CommentForm extends Component{
             return(
                 <div className="col-8 col-md-5 m-1">
                     <Card>
-                        <CardImg top src = {dish.image} alt={dish.name}/>
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle>{dish.name}</CardTitle>
                             <CardText>{dish.description}</CardText> 
@@ -126,7 +127,7 @@ class CommentForm extends Component{
             ); 
         }
 
-    function RenderDishComment({comments, addComment, dishId }){
+    function RenderDishComment({comments, postComment, dishId }){
         if(comments != null)
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -141,7 +142,7 @@ class CommentForm extends Component{
                             );
                         })}
                     </ul>
-                    <CommentForm dishId={dishId} addComment={addComment}/>
+                    <CommentForm dishId={dishId} postComment={postComment}/>
                 </div>
             );
         else
@@ -186,7 +187,7 @@ class CommentForm extends Component{
                     <div className="row row-content">
                         <RenderDish dish={props.dish}/>  
                         <RenderDishComment comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         dishId={props.dish.id}/> 
                     </div>
                 </div>
